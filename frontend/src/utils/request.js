1cfaee8bf74163f0f024fc906a348a2cc3beeb3f
Taimoor <ranatamoor067@gmail.com>
2025-03-07 15:55:37 +0500

    Your commit message

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL, // Use the environment variable
 });
 
 export default request;
